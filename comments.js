34d1899f0d64181e9a2e0c2dc67c6c25f1ad8240
José Medeiros <nicruo@gmail.com>
2023-09-21 08:39:08 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,20 +1,58 @@
-// Create a web server that can respond to requests for /comments.json
-// with a JSON representation of the comments list.
-// Don't forget to set the Content-Type header to application/json.
-// You'll need to read the comments.json file into memory on server start.
+// Create a web server
+// Node.js is a JavaScript runtime built on Chrome's V8 JavaScript engine.
+// Node.js uses an event-driven, non-blocking I/O model that makes it lightweight and efficient.
+// Node.js' package ecosystem, npm, is the largest ecosystem of open source libraries in the world.
+// Node.js is used by tens of thousands of companies in production, including Netflix, Uber and others.
 
-var http = require('http');
-var fs = require('fs');
-var port = 8080;
+// 1. Import the express module
+const express = require('express');
 
-var server = http.createServer(function (req, res) {
-  res.setHeader('Content-Type', 'application/json');
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  fs.readFile('./comments.json', 'utf8', function (err, data) {
-    if (err) throw err;
-    res.end(data);
-  });
+// 2. Create an instance of express
+const app = express();
+
+// 3. Create a port to listen to
+const port = 3000;
+
+// 4. Create a route
+app.get('/', (req, res) => {
+  res.send('Hello World!');
 });
 
-console.log('Listening on port ' + port);
-server.listen(port);
+// 5. Listen to the port
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
+
+// 6. Run the server
+// node comments.js
+// http://localhost:3000/
+// http://localhost:3000/?name=John
+// http://localhost:3000/?name=John&age=20
+// http://localhost:3000/?name=John&age=20&city=New%20York
+// http://localhost:3000/?name=John&age=20&city=New%20York&country=USA
+// http://localhost:3000/?name=John&age=20&city=New%20York&country=USA&job=Developer
+
+// 7. Install nodemon
+// npm install -g nodemon
+
+// 8. Run the server with nodemon
+// nodemon comments.js
+
+// 9. Install express-generator
+// npm install express-generator -g
+
+// 10. Create an express app
+// express express-app
+
+// 11. Install dependencies
+// cd express-app
+// npm install
+
+// 12. Run the server
+// npm start
+
+// 13. Install nodemon
+// npm install -g nodemon
+
+// 14. Run the server with nodemon
+// nodemon ./bin/www
